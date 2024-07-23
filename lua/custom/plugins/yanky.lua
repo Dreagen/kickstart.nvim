827427d926f4227d50d134389fb978ef71eb6935
Nick Allen <nick.allen@dataglide.co>
2024-07-23 10:56:07 +0100

    Add yanky plugin

---
+++ lua/custom/plugins/yanky.lua
@@ -0,0 +1,17 @@
+return {
+  'gbprod/yanky.nvim',
+  config = function()
+    local yanky = require 'yanky'
+    yanky.setup()
+
+    vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)')
+    vim.keymap.set({ 'n', 'x' }, 'P', '<Plug>(YankyPutBefore)')
+    vim.keymap.set({ 'n', 'x' }, 'gp', '<Plug>(YankyGPutAfter)')
+    vim.keymap.set({ 'n', 'x' }, 'gP', '<Plug>(YankyGPutBefore)')
+
+    vim.keymap.set('n', '<c-p>', '<Plug>(YankyPreviousEntry)')
+    vim.keymap.set('n', '<c-n>', '<Plug>(YankyNextEntry)')
+
+    require('telescope').load_extension 'yank_history'
+  end,
+}
