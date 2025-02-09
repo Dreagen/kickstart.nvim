85ab43a94e15910bfdc2f1deee96c72ddf48536d
Nick Allen <nick.allen@dataglide.co>
2025-02-09 14:12:10 +0000

    Update copilot setup

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -1,16 +1,36 @@
+-- return {
+--   {
+--     'CopilotC-Nvim/CopilotChat.nvim',
+--     dependencies = {
+--       { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+--       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+--     },
+--     opts = {
+--       debug = true, -- Enable debugging
+--       -- See Configuration section for rest
+--     },
+--     -- See Commands section for default commands if you want to lazy load on them
+--     keys = {
+--       {
+--         '<leader>co',
+--         ':CopilotChatOpen<CR>',
+--         mode = { 'n', 'o', 'x' },
+--       },
+--     },
+--   },
+-- }
+
 return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'canary',
     dependencies = {
-      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
     },
+    build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
+      -- See Configuration section for options
     },
-    -- See Commands section for default commands if you want to lazy load on them
     keys = {
       {
         '<leader>co',
@@ -18,5 +38,6 @@ return {
         mode = { 'n', 'o', 'x' },
       },
     },
+    -- See Commands section for default commands if you want to lazy load on them
   },
 }
