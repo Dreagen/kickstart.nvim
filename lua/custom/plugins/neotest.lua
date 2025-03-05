7b48bb7e2f1fccaa930a9872b76267ebf105f390
Nick Allen <nick.allen@dataglide.co>
2025-03-05 22:37:59 +0000

    Add rust test to neotest

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -6,12 +6,16 @@ return {
     'antoinemadec/FixCursorHold.nvim',
     'nvim-treesitter/nvim-treesitter',
     'Issafalcon/neotest-dotnet',
+    'rouge8/neotest-rust',
   },
   opts = function()
     return {
       adapters = {
         require 'neotest-dotnet' {
           framework = 'nunit', -- Specify your test framework
+          log_level = 'debug', -- Enable debug logging
+        },
+        require 'neotest-rust' {
           log_level = 'debug', -- Enable debug logging
         },
       },
