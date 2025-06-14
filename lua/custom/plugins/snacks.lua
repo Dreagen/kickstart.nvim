79564cd9f15f1d77c5ec90a16d62642c16e6b71b
Nick Allen <nick.allen@dataglide.co>
2025-06-14 09:48:53 +0100

    Start adding snacks lua - not finished!

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,43 @@
+-- lazy.nvim
+return {
+  'folke/snacks.nvim',
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      -- your picker configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      -- Top Pickers & Explorer
+      {
+        '<leader>j',
+        function()
+          Snacks.picker.smart()
+        end,
+        desc = 'Smart Find Files',
+      },
+      {
+        '<leader>,',
+        function()
+          Snacks.picker.buffers()
+        end,
+        desc = 'Buffers',
+      },
+      {
+        '<leader>/',
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = 'Grep',
+      },
+      {
+        '<leader>:',
+        function()
+          Snacks.picker.command_history()
+        end,
+        desc = 'Command History',
+      },
+    },
+  },
+}
