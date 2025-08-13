18d45947c0fdb3d72df173d4397d4cfe2c59965c
Nick Allen <nick.allen@dataglide.co>
2025-08-14 00:05:42 +0100

    Update easy dotnet configuration

--- lua/custom/plugins/easy-dotnet.lua
+++ lua/custom/plugins/easy-dotnet.lua
@@ -2,6 +2,16 @@ return {
   'GustavEikaas/easy-dotnet.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
   config = function()
-    require('easy-dotnet').setup()
+    local dotnet = require 'easy-dotnet'
+    dotnet.setup {
+      test_runner = {
+        viewmode = 'float',
+        enable_buffer_test_execution = true,
+      },
+      picker = 'snacks',
+      background_scanning = true,
+    }
+
+    vim.keymap.set('n', '<leader>dt', dotnet.testrunner, { desc = 'Open .NET Test Runner' })
   end,
 }
