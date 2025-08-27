896a74c93463cbe873c9d664bd72b89e4675b4c6
Nick Allen <nick.allen@dataglide.co>
2025-08-27 15:03:41 +0100

    Add roslyn lsp to mason configuration

--- init.lua
+++ init.lua
@@ -688,8 +688,13 @@ require('lazy').setup({
       --    :Mason
       --
       --  You can press `g?` for help in this menu.
-      require('mason').setup()
-
+      -- require('mason').setup()
+      require('mason').setup {
+        registries = {
+          'github:mason-org/mason-registry',
+          'github:Crashdummyy/mason-registry',
+        },
+      }
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
